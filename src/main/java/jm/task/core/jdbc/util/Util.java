4ba50c693b83718bf0f09eb5ec6741ff0ec4641a
psyhotel <sative2010@yandex.ru>
2022-12-16 09:12:50 +0500

    2/1/1/4 fix3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 public class Util {
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "root";
-
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     public static Connection connection;
 
